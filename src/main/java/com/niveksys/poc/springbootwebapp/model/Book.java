17c8964f2521093f274ad7769763a10ac94565c5
Kevin Tsang <niveksys@gmail.com>
2020-12-30 21:36:56 +0800

    Add Thymeleaf Templates

--- src/main/java/com/niveksys/poc/springbootwebapp/model/Book.java
+++ src/main/java/com/niveksys/poc/springbootwebapp/model/Book.java
@@ -78,7 +78,7 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book [authors=" + authors + ", id=" + id + ", isbn=" + isbn + ", title=" + title + "]";
+        return "Book [id=" + id + ", isbn=" + isbn + ", title=" + title + "]";
     }
 
     @Override
